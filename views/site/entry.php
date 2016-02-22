e5aa394ce38ffc5d1c54589e8de9d2b4843e9398
somekey <ithbanykey@gmail.com>
2016-02-22 22:53:28 +0200

    Added last form files view and model

---
+++ views/site/entry.php
@@ -0,0 +1,12 @@
+﻿<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+<?php $form = ActiveForm::begin(); ?>
+<?= $form->field($model, 'name')->label('Будь-ласка введіть Ваше ім\'я') ?>
+<?= $form->field($model, 'email') ?>
+<div class="form-group">
+<?= Html::submitButton('Отправить', ['class' => 'btn btn-primary'])
+?>
+</div>
+<?php ActiveForm::end(); ?>
